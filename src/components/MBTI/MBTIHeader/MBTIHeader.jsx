4b9ed525e3fe15c95e5156267a3f85571c29ab4a
Van Vi <pvanvi2508@gmail.com>
2024-03-21 00:27:34 +0700

    fix MBTI and profile

--- src/components/MBTI/MBTIHeader/MBTIHeader.jsx
+++ src/components/MBTI/MBTIHeader/MBTIHeader.jsx
@@ -1,9 +1,9 @@
-import "../../MBTI/MBTIHeader/MBTIHeader.model.css";
-import { FaArrowRight } from "react-icons/fa";
-import styled, { keyframes } from "styled-components";
-import { useEffect, useRef } from "react";
-import { SliderMBTI } from "../../../data/MBTI/MBTI";
-import { Link } from "react-router-dom";
+import '../../MBTI/MBTIHeader/MBTIHeader.model.css';
+import { FaArrowRight } from 'react-icons/fa';
+import styled, { keyframes } from 'styled-components';
+import { useEffect, useRef } from 'react';
+import { SliderMBTI } from '../../../data/MBTI/MBTI';
+import { Link } from 'react-router-dom';
 
 const marqueeAnimation = keyframes`
  from {
@@ -14,91 +14,89 @@ const marqueeAnimation = keyframes`
   }
 `;
 const MarqueeContainer = styled.div`
-  align-items: flex-start;
-  animation: ${marqueeAnimation} 84s linear infinite;
-  display: flex;
-  gap: 24px;
-  position: absolute;
-  will-change: transform;
-  overflow: hidden;
-  width: max-content;
-  animation-fill-mode: both;
-  animation-timing-function: ease-in-out;
+    align-items: flex-start;
+    animation: ${marqueeAnimation} 84s linear infinite;
+    display: flex;
+    gap: 24px;
+    position: absolute;
+    will-change: transform;
+    overflow: hidden;
+    width: max-content;
+    animation-fill-mode: both;
+    animation-timing-function: ease-in-out;
 `;
 const MarqueeLink = styled.a`
-  background: white;
-  align-items: center;
-  border-radius: 20px;
-  cursor: pointer;
-  display: flex;
-  gap: 16px;
-  position: relative;
-  width: 401px;
-  &:hover {
-    border: 2px solid yellow;
-    overflow: visible;
-  }
+    background: white;
+    align-items: center;
+    border-radius: 20px;
+    cursor: pointer;
+    display: flex;
+    gap: 16px;
+    position: relative;
+    width: 401px;
+    &:hover {
+        border: 2px solid yellow;
+        overflow: visible;
+    }
 `;
 
 const MBTIHeader = () => {
-  const marqueeRef = useRef(null);
+    const marqueeRef = useRef(null);
 
-  useEffect(() => {
-    const handleMouseEnter = () => {
-      marqueeRef.current.style.animationPlayState = "paused";
-    };
+    useEffect(() => {
+        const handleMouseEnter = () => {
+            marqueeRef.current.style.animationPlayState = 'paused';
+        };
 
-    const handleMouseLeave = () => {
-      marqueeRef.current.style.animationPlayState = "running";
-    };
+        const handleMouseLeave = () => {
+            marqueeRef.current.style.animationPlayState = 'running';
+        };
 
-    marqueeRef.current.addEventListener("mouseenter", handleMouseEnter);
-    marqueeRef.current.addEventListener("mouseleave", handleMouseLeave);
-  }, []);
+        marqueeRef.current.addEventListener('mouseenter', handleMouseEnter);
+        marqueeRef.current.addEventListener('mouseleave', handleMouseLeave);
+    }, []);
 
-  return (
-    <div>
-      <div className="mbti-bg-header">
-        <div id="mbti-header" className="mbti-header">
-          <div className="mbti-header__top-content">
-            <div className="mbti-header__top-content--title-group">
-              <h1 className="text-white text-5xl font-extrabold tighter leading-10 text-center uppercase ">
-                Trắc nghiệm tính cách MBTI
-              </h1>
+    return (
+        <div>
+            <div className="mbti-bg-header">
+                <div id="mbti-header" className="mbti-header">
+                    <div className="mbti-header__top-content">
+                        <div className="mbti-header__top-content--title-group">
+                            <h1 className="text-5xl font-extrabold leading-10 text-center text-white uppercase tighter ">
+                                Trắc nghiệm tính cách MBTI
+                            </h1>
+                        </div>
+                        <Link to="/mbtiquizz" className="mbti-header__top-content--button">
+                            Làm bài test
+                            <FaArrowRight size={28} color="white" />
+                        </Link>
+                    </div>
+                    <div className="mbti-header__personalities">
+                        <MarqueeContainer ref={marqueeRef}>
+                            {SliderMBTI.map((mbtiCard, i) => (
+                                <MarqueeLink key={i}>
+                                    <div className="mbti-header__personality--image lazyloaded">
+                                        <img
+                                            className="mbt-header__img--intro"
+                                            src={mbtiCard.img}
+                                            alt={mbtiCard.altText}
+                                        />
+                                    </div>
+
+                                    <div className="mbti-header__personality--content">
+                                        <div className="mbti-header__personality--content-title">{mbtiCard.title}</div>
+                                        <div className="mbti-header__personality--content-short-description">
+                                            {mbtiCard.detail}
+                                        </div>
+                                    </div>
+                                </MarqueeLink>
+                            ))}
+                        </MarqueeContainer>
+                    </div>
+                </div>
             </div>
-            <Link to="/mbtiquizz" className="mbti-header__top-content--button">
-              Làm bài test
-              <FaArrowRight size={28} color="white" />
-            </Link>
-          </div>
-          <div className="mbti-header__personalities">
-            <MarqueeContainer ref={marqueeRef}>
-              {SliderMBTI.map((mbtiCard, i) => (
-                <MarqueeLink key={i}>
-                  <div className="mbti-header__personality--image lazyloaded">
-                    <img
-                      className="mbt-header__img--intro"
-                      src={mbtiCard.img}
-                      alt={mbtiCard.altText}
-                    />
-                  </div>
-
-                  <div className="mbti-header__personality--content">
-                    <div className="mbti-header__personality--content-title">
-                      {mbtiCard.title}
-                    </div>
-                    <div className="mbti-header__personality--content-short-description">
-                      {mbtiCard.detail}
-                    </div>
-                  </div>
-                </MarqueeLink>
-              ))}
-            </MarqueeContainer>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default MBTIHeader;
--- src/page/DetailMajors/index.jsx
+++ src/page/DetailMajors/index.jsx
@@ -1,36 +1,58 @@
-import { Link } from "react-router-dom";
-import Footer2 from "../../components/Footer2/Footer2";
-import Navbar from "../../components/HomePage/header/Navbar";
-
-import css from '../DetailMajors/DetailMajors.module.scss'
+import Footer2 from '../../components/Footer2/Footer2';
+import Navbar from '../../components/HomePage/header/Navbar';
 
 export default function DetailMajors() {
-  return (
-    <>
-        <Navbar/>
-           <div className="container px-0 py-0">
-             <div className={css.banner}>
-                <div className="flex flex-col text-center mb-2 z-10">
-                    <h1 className="text-3xl text-white font-semibold cursor-pointer">Danh Sách Ngành học</h1>
-                    <div className="text-xl text-white font-normal mt-3">
-                        <Link className="hover:underline" to={'/Homepage'}>Trang Chủ</Link> &gt; <span className="hover:underline">Công nghệ thông tin</span>
+    return (
+        <>
+            <Navbar />
+            <div className="bg-white">
+                <main className="container mx-auto bg-white mt-4 px-10">
+                    <div>
+                        <h1 className=" text-center text-3xl font-medium pt-4">Kỹ thuật phần mềm</h1>
+                        <div className="flex items-center justify-between  py-0">
+                            <p>
+                                <span className="text-xl font-semibold italic ">mã ngành:</span> 09001
+                            </p>
+                        </div>
                     </div>
-                </div>
-             </div>
-             <div className="grid grid-cols-4 mt-2">
-                  <nav className="bg-white col-span-1 rounded-lg">
-                    <ul className="flex flex-col">
-                      <li className="text-xl cursor-pointer p-2 rounded-md font-normal hover:bg-silver">Kỹ thuật phần mềm</li>
-                      <li className="text-xl cursor-pointer p-2 rounded-md font-normal hover:bg-silver">Mạng máy tính</li>
-                      <li className="text-xl cursor-pointer p-2 rounded-md font-normal hover:bg-silver">Truyên thông và dữ liệu</li>
+                    <ul className="">
+                        <p className="font-bold text-[18px] italic">1. Giới thiệu:</p>
+                        <li className="text-base text-justify mb-4 tracking-widest leading-7">
+                            Chúng ta không thể sống trong một thế giới hiện đại mà không cần dùng đến phần mềm. Hầu hết
+                            các thiết bị điện tử hiện đại ngày nay đều được điều khiển bằng phần mềm. Nền sản xuất và
+                            phân phối công nghiệp; dịch vụ tài chính ngân hàng, chứng khoán, bất động sản, … đều ứng
+                            dụng phần mềm máy tính để xử lý hầu hết các công việc. Đặc biệt, trong lĩnh vực giải trí như
+                            trò chơi máy tính, điện ảnh và truyền hình đều sử dụng các phần mềm chuyên dụng để xử lý các
+                            công việc phức tạp. Từ đó cho thấy rằng phần mềm rất cần thiết cho hầu hết các hoạt động của
+                            đời sống kinh tế và xã hội.
+                        </li>
+                        <li className="text-base text-justify mb-4 tracking-widest leading-7">
+                            Ngành Kỹ thuật phần mềm đào tạo những kiến thức liên quan đến quy trình phát triển phần mềm
+                            một cách chuyên nghiệp nhằm tạo ra sản phẩm phần mềm đạt chất lượng cao, đáp ứng các nhu cầu
+                            nghiệp vụ cụ thể trong nền sản xuất của xã hội. Sinh viên học ngành này được trang bị các
+                            kiến thức chuyên sâu về công nghiệp phần mềm, bao gồm: quy trình phát triển phần mềm, kỹ
+                            năng vận dụng các công cụ phần mềm vào việc hỗ trợ phát triển các phần mềm khác. Sinh viên
+                            cũng được trang bị các kiến thức cần thiết liên quan đến các pha thực hiện trong một dự án
+                            phần mềm như: thu thập yêu cầu, phân tích, thiết kế, lập trình, kiểm thử, vận hành và bảo
+                            trì phần mềm.
+                        </li>
                     </ul>
-                  </nav>
-                  <div className="col-span-3">
-                    
-                  </div>
-                </div>
-           </div>
-        <Footer2/>
-    </>
-  )
+                    <div className="">
+                        <p className="font-bold text-[18px] italic">2.Ngành đào tạo:</p>
+                        <ul>
+                            <li className="text-base text-justify mb-4 tracking-widest leading-7">
+                                <span className="font-bold italic">Kỹ Thuật phần mềm: </span>
+                                <span>0908986</span>
+                            </li>
+                            <li className="text-base text-justify mb-4 tracking-widest leading-7">
+                                <span className="font-bold italic">Kỹ Thuật phần mềm: </span>
+                                <span>0908986</span>
+                            </li>
+                        </ul>
+                    </div>
+                </main>
+            </div>
+            <Footer2 />
+        </>
+    );
 }
